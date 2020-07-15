dca0023ee3d792c4e4083786427c447901aa980d
StefansArya <stefansarya1@gmail.com>
2020-07-15 22:07:55 +0700

    Fix sf.dom for append and prepend element

--- src/sf-dom.js
+++ src/sf-dom.js
@@ -314,7 +314,7 @@ class DOMList{
 		return this;
 	}
 	append(element){
-		if(element.constructor === Array || element.classList === void 0){
+		if(element.constructor === Array){
 			for (var i = 0; i < element.length; i++)
 				this[0].append(element[i]);
 		}
@@ -326,7 +326,7 @@ class DOMList{
 		return this;
 	}
 	prepend(element){
-		if(element.constructor === Array || element.classList === void 0){
+		if(element.constructor === Array){
 			for (var i = 0; i < element.length; i++)
 				this[0].prepend(element[i]);
 		}
