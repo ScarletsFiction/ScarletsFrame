843b557360faf6c159ec1ca73422382ec11c2d5d
StefansArya <stefansarya1@gmail.com>
2020-10-01 03:34:42 +0700

    Add support for TypeScript

---
+++ dist/scarletsframe.min.d.ts
@@ -0,0 +1,255 @@
+// Type definitions for ScarletsFrame
+// Project: https://github.com/ScarletsFiction/ScarletsFrame
+// Definitions by: StefansArya <https://github.com/stefansarya>
+
+export declare function sf(obj: Function | HTMLElement): HTMLElement | object | null | void;
+export declare namespace sf {
+    /**
+     * Define new <sf-m name=""> element handler
+     * @param options Model configuration, this can be the 3rd parameter
+     * @param scope Your extendable class or callable function
+     */ export function model(name: String, options?: Function | ModelOptions | ((self: SFModel) => void), scope?: Function | ((self: SFModel) => void)): SFModel;
+    export namespace model {
+        /**
+         * Get the index of RepeatedElement on DOM
+         * @param element single RepeatedElement
+         */ function index(element: HTMLElement): number;
+    }
+    /**
+     * Define new <sf-m name=""> element handler
+     * @param options Model configuration, this can be the 3rd parameter
+     * @param scope Your extendable class or callable function
+     */ export function component(name: String, options?: Function | ModelOptions | ((self: SFModel) => void), scope?: Function | ((self: SFModel) => void)): ComponentList;
+    export namespace component {
+        /**
+         * Define new template for a component, this is optional
+         * If the you already write the content of the component
+         * in the DOM then you can skip this
+         * @param name Component name
+         * @param template The template content for the component
+         */ function html(name: String, template: String | HTMLElement | TemplateOptions): void;
+    }
+    export { API };
+    export namespace loader {
+        function onProgress(callback: (loaded?: number, total?: number) => void): void;
+        function css(list: string[]): void;
+        function js(list: string[]): void;
+    }
+    export { Space as space };
+    export function url(): string;
+    export namespace url {
+        const data: string[];
+        const hashes: {};
+        const paths: string;
+        function parse(url: string): URLData;
+    }
+    export function lang(): void;
+    export namespace lang {
+        export const list: {};
+        export const interpolate: {};
+        const _default: string;
+        export { _default as default };
+        export const serverURL: string;
+        export function add(lang: string, obj: object): void;
+        export function get(path: string, obj?: object | Function, callback: Function): void;
+        export function init(el: HTMLElement): void;
+    }
+    export namespace window {
+        const list_1: {};
+        export { list_1 as list };
+        export function destroy(id: string): void;
+        export function create(options: WindowOptions, onLoaded?: Function): void;
+        export function source(lists: HTMLElement[], ev: EventTarget): HTMLElement;
+    }
+    export function hotReload(mode: number): void;
+    export { View as views };
+    export function dom(selector: string | Function | HTMLElement | HTMLElement[], context: HTMLElement | DOMList): void | DOMList;
+    export namespace dom {
+        const fn: DOMList;
+    }
+}
+declare class DOMList extends Array<HTMLElement> {
+    constructor(arrayLength?: number);
+    constructor(arrayLength: number);
+    constructor(...items: any[]);
+    parent(selector: HTMLElement): DOMList;
+    prev(selector: HTMLElement): DOMList;
+    prevAll(selector: HTMLElement): DOMList;
+    next(selector: HTMLElement): DOMList;
+    nextAll(selector: HTMLElement): DOMList;
+    children(selector: HTMLElement): DOMList;
+    remove(): DOMList;
+    empty(): DOMList;
+    addClass(name: string): DOMList;
+    removeClass(name: string): DOMList;
+    toggleClass(name: string): DOMList;
+    hasClass(name: string): boolean;
+    prop(name: string, value: any): DOMList;
+    attr(name: string, value: string): DOMList;
+    removeAttr(name: string, value: string): DOMList;
+    css(name: string, value: string): DOMList;
+    on(event: string, selector?: Function | string, callback?: Function, options?: object): DOMList;
+    off(event: string, selector?: Function | string, callback?: Function, options?: object): DOMList;
+    once(event: string, selector?: Function | string, callback?: Function): DOMList;
+    trigger(event: string, data: any, direct: boolean): DOMList;
+    animateKey(name: string, callback: Function, duration: number): DOMList;
+    each(callback: (index?: number, value?: any) => void): DOMList;
+    data(key: string, value: any): DOMList;
+    removeData(key: string): DOMList;
+    append(element: HTMLElement | DOMList): DOMList;
+    prepend(element: HTMLElement | DOMList): DOMList;
+    eq(index: number, count: number): DOMList;
+    inserAfter(element: HTMLElement | DOMList): DOMList;
+    inserBefore(element: HTMLElement | DOMList): DOMList;
+    text(text?: string): DOMList | string;
+    html(text?: string): DOMList | string;
+    val(val?: string): DOMList | string;
+    click(data: any): DOMList;
+    blur(data: any): DOMList;
+    focus(data: any): DOMList;
+    focusin(data: any): DOMList;
+    focusout(data: any): DOMList;
+    keyup(data: any): DOMList;
+    keydown(data: any): DOMList;
+    keypress(data: any): DOMList;
+    submit(data: any): DOMList;
+    change(data: any): DOMList;
+    mousedown(data: any): DOMList;
+    mousemove(data: any): DOMList;
+    mouseup(data: any): DOMList;
+    mouseenter(data: any): DOMList;
+    mouseleave(data: any): DOMList;
+    mouseout(data: any): DOMList;
+    mouseover(data: any): DOMList;
+    touchstart(data: any): DOMList;
+    touchend(data: any): DOMList;
+    touchmove(data: any): DOMList;
+    resize(data: any): DOMList;
+    scroll(data: any): DOMList;
+}
+declare class SFModel {
+    /** Component or Model's container element list */
+    $el: DOMList[];
+}
+interface ModelOptions {
+    /** Your extendable class */
+    extend: Function;
+}
+/** Constructor that can be used for creating new element */
+declare class SFComponent {
+    /**
+     * @param $item Object that being passed when contructing new component scope
+     * @param namespace If this was boolean this will be asScope parameter
+     * @param asScope Use $item as component scope
+     */
+    constructor($item?: object, namespace?: object | boolean, asScope?: boolean);
+}
+/** Component scope collection */
+declare class ComponentList extends Array<SFModel> {
+    constructor(arrayLength?: number);
+    constructor(arrayLength: number);
+    constructor(...items: SFModel[]);
+    element: SFComponent;
+}
+interface TemplateOptions {
+    /** Template path from window.templates */
+    template: String;
+}
+declare enum HTTPMethod {
+    GET = 0,
+    POST = 1
+}
+interface SFSpaceOptions {
+    /** Template path in window.templates */
+    templatePath: string;
+}
+declare class SFSpace {
+    model: {
+        (name: String, options?: Function | ModelOptions | ((self: SFModel) => void), scope?: Function | ((self: SFModel) => void)): SFModel;
+        index(element: HTMLElement): number;
+    };
+    component: {
+        (name: String, options?: Function | ModelOptions | ((self: SFModel) => void), scope?: Function | ((self: SFModel) => void)): ComponentList;
+        html(name: String, template: HTMLElement | String | TemplateOptions): void;
+    };
+    /** Destroy sf-space */
+    destroy(): void;
+}
+declare class URLData {
+    data: string[];
+    hashes: {};
+    paths: string;
+}
+interface WindowOptions {
+    id?: string;
+    title?: string;
+    width?: number;
+    height?: number;
+    route?: string;
+    templateHTML?: HTMLElement;
+    templatePath?: string;
+    templateURL?: string;
+}
+declare class API {
+    /** @param url API Root url, ex: https://example.com/api/v2 */
+    constructor(url: string);
+    /** Set X-Authentication: AccessToken bearer on the header */
+    accessToken: boolean;
+    /** Set any request method into _method with inside of data and send via POST method */
+    mask: boolean;
+    /**
+     * @param url API Path url, ex: /create/post
+     * @param data Data to be send
+     */
+    get(url: string, data?: object): void;
+    post(url: string, data?: object): void;
+    delete(url: string, data?: object): void;
+    put(url: string, data?: object): void;
+    upload(url: string, data: FormData): void;
+    request(method: HTTPMethod, url: string, data?: object, beforeSend?: Function): void;
+}
+declare class Space {
+    /**
+     * @param namespace name for <sf-space name>
+     * @param options optional if the element was empty on the DOM
+     */
+    constructor(namespace: string, options?: SFSpaceOptions);
+    getScope(index: number): void;
+    createHTML(index: number): HTMLElement;
+}
+declare class View {
+    /**
+     * Page routing for ScarletsFrame
+     * @param selector Specify custom element as views container
+     * @param name if empty then use the main URL path
+     */
+    constructor(selector: string, name?: string);
+    /** Enable dynamic script evaluation that was loaded from new page */
+    dynamicScript: boolean;
+    /** Last views path */
+    lastPath: string;
+    /** Previous page element after routed to new page */
+    lastDOM: HTMLElement | null;
+    /** Current page element after routed to new page */
+    currentDOM: HTMLElement | null;
+    /** Related page element parents after routed to new page */
+    relatedDOM: HTMLElement[];
+    /** Views data from URL or page data */
+    data: {};
+    /** Listen to an page event */
+    on(event: string, callback?: Function): void;
+    /**
+     * Stop listen to an page event
+     * @param event if empty then remove all event
+     * @param callback if empty then remove all callback for the specified event
+     */
+    off(event?: string, callback?: Function): void;
+    /** Add page routes */
+    addRoute(routes: object): void;
+    /** Remove path */
+    removeRoute(path: string): void;
+    /** Route pages to a new path */
+    goto(path: string, data?: Function | object, method?: Function | HTTPMethod, callback?: Function): void;
+}
+
+export default sf;
