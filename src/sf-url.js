c05f0cdb2fe33b707407fb0ed48615d9415ac8be
StefansArya <stefansarya1@gmail.com>
2020-11-16 13:21:51 +0700

    Improve the parser and add feature for saving data on the URL query

--- src/sf-url.js
+++ src/sf-url.js
@@ -1,103 +1,165 @@
 ;(function(){
 const self = sf.url = function(){
 	// Hash
-	let hash_ = '';
-	let hash = self.hash;
-	for(let keys in hash){
-		if(hash[keys] === '/') continue;
-		hash_ += `#${keys}${hash[keys]}`;
+	let _hash = '';
+	let hash = self.routes;
+	for(let key in hash){
+		if(hash[key] === '/') continue;
+		_hash += `#${key}${hash[key]}`;
 	}
 
 	// Query
-	let query_ = '';
+	let _query = '';
 	let query = self.query;
-	for(let keys in query){
-		if(query_.length === 0)
-			query_ += '?';
-		else query_ += '&';
+	for(let key in query)
+		_query += `${(_query.length === 0 ? '?' : '&')}${key}=${encodeURI(query[key])}`;
 
-		query_ += `${keys}=${encodeURI(query[keys])}`;
+	// Data
+	let _data = '';
+	let data = self.data;
+	for(let key in data){
+		var dat = data[key];
+
+		if(dat !== null){
+			if(dat.constructor === Array || dat.constructor === String)
+				validateURLData(dat);
+
+			if(dat.constructor === Array)
+				dat = dat.join(',');
+			_data += `;${key}:${dat}`;
+		}
+		else _data += `;${key}`;
 	}
 
-	const data_ = `|${self.data.join('|')}`;
-	return self.path + query_ + hash_ + (data_.length !== 1 ? data_ : '');
+	return `${self.path}${_query}${_hash}${
+		(_data.length === 0 ? '' : `#${encodeURI(_data)}`)
+	}`;
 };
 
-self.hash = {};
-self.query = {};
-self.data = [];
-self.path = '/';
+self.path = '/'; // Main URL path without hash/query/data
+self.routes = {}; // Used for sf-views for multiple hash routes
+self.query = {}; // GET query parameter on the URL
+self.data = {}; // {UniqID: [String, ...], UniqID: String}
+
+// Shortcut
+const history = window.history;
+const location = window.location;
 
 // Push into latest history
 self.push = function(){
-	window.history.pushState((window.history.state || 0) + 1, '', self());
+	history.pushState((history.state || 0) + 1, '', self());
 	self.trigger();
 }
 
 // Remove next history and change current history
 self.replace = function(){
-	window.history.replaceState(window.history.state, '', self());
+	history.replaceState(history.state, '', self());
 	self.trigger();
 }
 
-self.get = function(name, index){
-	self.parse();
-
-	if(name.constructor === Number)
-		return self.path.split('/')[name+1];
-
-	if(hash[name] === void 0)
-		return;
-
-	return hash[name].split('/')[index+1];
-}
-
+// If url === true, it will parse current URL save the data into sf.url
+// If url is String, it will parse the String and create new object to save the data
 self.parse = function(url){
-	if(url !== void 0){
-		const data = {hash:{}, query:{}};
-		data.data = url.split('|');
+	let obj, URLQuery, URLHash, URLData;
 
-		var hash_ = data.data.shift().split('#');
-		for (var i = 1; i < hash_.length; i++) {
-			var temp = hash_[i].split('/');
-			data.hash[temp.shift()] = `/${temp.join('/')}`;
+	if(url === true){
+		obj = self;
+		obj.path = location.pathname;
+
+		obj.query = {};
+		obj.routes = {};
+		obj.data = {};
+
+		URLQuery = location.search.slice(1);
+		[URLHash, URLData] = location.hash.split('#;');
+
+		if(URLHash.length !== 0)
+			URLHash = URLHash.slice(1).split('#');
+		else URLHash = void 0;
+	}
+	else{
+		obj = {routes:{}, query:{}, data:{}};
+		URLQuery = url.split('?');
+
+		// /URLPath#URLHash#;URLData
+		if(URLQuery.length === 1){
+			[URLHash, URLData] = URLQuery[0].split('#;');
+		    URLQuery = void 0;
+
+		    if(URLHash.includes('#')){
+		      URLHash = URLHash.split('#');
+		      obj.path = URLHash.shift();
+		    }
+		    else{
+		        obj.path = URLHash;
+		        URLHash = void 0;
+		    }
 		}
+		// /URLPath?URLQuery#URLHash#;URLData
+		else{
+			obj.path = URLQuery[0];
+			[URLHash, URLData] = URLQuery[1].split('#;');
 
-		var query_ = hash_[0].split('?');
-		if(query_.length !== 1){
-			query_ = query_[1].split('&');
-			for (var i = 0; i < query_.length; i++) {
-				var temp = query_[i].split('=');
-				data.query[temp[0]] = decodeURI(temp[1]);
-			}
+		    if(URLHash.includes('#')){
+		        URLHash = URLHash.split('#');
+		        URLQuery = URLHash.shift();
+		    }
+		    else{
+		        URLQuery = URLHash;
+		        URLHash = void 0;
+		    }
 		}
-
-		// Path
-		data.path = url.split('#')[0].split('?')[0];
-		return data;
 	}
 
-	self.data = window.location.hash.split('|');
+	let query = obj.query;
+	let routes = obj.routes;
+	let data = obj.data;
 
-	let hash = self.hash = {};
-	var hash_ = self.data.shift().split('#');
-	for (var i = 1; i < hash_.length; i++) {
-		var temp = hash_[i].split('/');
-		hash[temp.shift()] = `/${temp.join('/')}`;
-	}
-
-	let query = self.query = {};
-	if(window.location.search.length !== 0){
-		var query_ = window.location.search.slice(1).split('&');
-		for (var i = 0; i < query_.length; i++) {
-			var temp = query_[i].split('=');
+	if(URLQuery !== void 0){
+		URLQuery = URLQuery.split('&');
+		for (var i = 0; i < URLQuery.length; i++) {
+			var temp = URLQuery[i].split('=');
 			query[temp[0]] = decodeURI(temp[1]);
 		}
 	}
 
-	// Path
-	self.path = window.location.pathname;
-	return self;
+	if(URLHash !== void 0){
+		for (var i = 0; i < URLHash.length; i++) {
+			const temp = URLHash[i].split('/');
+			routes[temp.shift()] = `/${temp.join('/')}`;
+		}
+	}
+
+	if(URLData !== void 0){
+		URLData = decodeURI(URLData).split(';');
+		for (var i = 0; i < URLData.length; i++) {
+			const temp = URLData[i].split(':');
+			data[temp[0]] = temp.length === 1 ? null : temp[1].split(',');
+		}
+	}
+
+	return obj;
+}
+
+const URLDataValidator = /[,:;]/;
+function validateURLData(dat){
+	if(dat.constructor === Array){
+		for (var i = 0; i < dat.length; i++) {
+			const data = dat[i];
+			if(data.constructor === Number) continue;
+
+			if(URLDataValidator.test(data)){
+				console.log('URLData got:', dat);
+				throw new Error("URL data must not contain , : or ; symbol");
+			}
+		}
+		return;
+	}
+
+	if(URLDataValidator.test(dat)){
+		console.log('URLData got:', dat);
+		throw new Error("URL data must not contain , : or ; symbol");
+	}
 }
 
 let listener = {query:[], hash:[], path:[], data:[]};
@@ -136,6 +198,5 @@ self.trigger = function(){
 	}
 }
 
-self.parse();
-
+self.parse(true);
 })();
