c517f8d670594eb1fe35c3046af06246072fa590
StefansArya <stefansarya1@gmail.com>
2020-11-19 06:49:10 +0700

    Remove unneeded variable

--- src/sf-virtual_scroll.js
+++ src/sf-virtual_scroll.js
@@ -4,7 +4,7 @@
 
 // ToDo: add sf$scrollPos and sf$heightPos
 const ElementManipulatorProxy = internal.EMP;
-const ElementManipulator = internal.EM;
+// const ElementManipulator = internal.EM;
 const VSM_Threshold = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
 let virtualScrolling = false;
 
