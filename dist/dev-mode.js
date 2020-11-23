bcd29557eba25b4c0c956818d20c9c4eef64497b
StefansArya <stefansarya1@gmail.com>
2020-11-24 04:19:18 +0700

    Improve dev tools

--- dist/dev-mode.js
+++ dist/dev-mode.js
@@ -1,25 +1,145 @@
+// For browser interface
+var SFDevSpace = new sf.space('sf_devmode');
+var SFDevMode = SFDevSpace.component('sf-dev-mode', {
+	html:`
+	<div class="sf-shadow-mark" style="
+		display: {{ hasShadow ? '' : 'none' }};
+		transform: translate({{ x }}px, {{ y }}px);
+		width: {{ width }}px;
+		height: {{ height }}px;
+	"></div>
+	<div class="sf-side-info {{ sideOpened ? 'opened' : ''}} {{ haveList ? 'have-list' : ''}}">
+		<div class="title">{{ message }}</div>
+		<div class="list">
+			<sf-model-info sf-each="val in models" sf-as-scope/>
+		</div>
+	</div>`
+}, function(My){
+	My.hasShadow = false;
+	My.x = My.y = 0;
+	My.width = My.height = 0;
+	My.sideOpened = true;
+	My.sideOpenLock = false;
+	My.haveList = false;
+	My.message = "Development Mode Enabled";
+	My.models = [];
+
+	$('body').on('pointermove', function(e){
+		if(e.ctrlKey && e.altKey){
+			const el = sf(e.target, true);
+			if(!el){
+				My.hasShadow = false;
+				My.message = "No Frame Detected";
+
+				if(My.haveList){
+					My.haveList = false;
+					My.models.splice(0);
+				}
+				return;
+			}
+
+			const Rect = el.getBoundingClientRect();
+			My.x = Rect.x;
+			My.y = Rect.y;
+			My.width = Rect.width;
+			My.height = Rect.height;
+			My.hasShadow = true;
+			My.sideOpened = true;
+			My.haveList = true;
+			My.message = "Inspecting Element";
+			scanElementFrame(e);
+		}
+		else{
+			if(My.sideOpenLock === false)
+				My.models.splice(0);
+
+			if(My.models.length === 0){
+				My.haveList = false;
+				My.sideOpened = false;
+			}
+
+			My.hasShadow = false;
+		}
+	});
+
+	function scanElementFrame(e){
+		var nested = 0;
+		var frameList = [];
+
+		var modelEl = e.target;
+		while(modelEl = sf(modelEl, true)){
+			frameList.push({
+				name: modelEl.sf$controlled || '{embedded template}',
+				nested,
+				model: modelEl.model,
+				modelEl,
+				ref: {
+					script: modelEl.model.$el?.$scopeFunc || modelEl.sf$collection?.$scopeFunc
+				},
+			});
+
+			modelEl = modelEl.parentNode;
+			nested++;
+		};
+
+		My.models.assign(frameList);
+	}
+
+	My.init = function(){
+		setTimeout(()=> {
+			My.sideOpened = false;
+		}, 3000);
+	}
+});
+
+// sf-each-> sf-as-scope enabled
+SFDevSpace.component('sf-model-info', {
+	html:`
+	<div>{{ name }}</div>
+`
+}, function(My, root){
+	// My.name = $item.name;
+});
+
+// Add to body when DOM was finished loading
+sf.dom(function(){
+	$('body').append('<sf-space sf_devmode><sf-dev-mode/></sf-space>');
+	console.log(SFDevMode[0]);
+});
+
+// For browser console
 sf.dom(function(){
 	var $ = sf.dom;
-	$('body').on('click', function(e){
+	function preventAltCtrlClick(e){
+		if(e.ctrlKey && e.altKey){
+			e.preventDefault();
+			e.stopImmediatePropagation();
+			e.stopPropagation();
+		}
+	}
+
+	$('body')
+	.on('pointerdown', function(e){
 		if(e.ctrlKey && e.altKey){
 			e.preventDefault();
 			e.stopImmediatePropagation();
 			e.stopPropagation();
 
-			var nested = 0;
+			var component = SFDevMode[0];
+			const frameList = component.models;
 
-			var modelEl = e.target;
-			while(modelEl = sf(modelEl, true)){
-				const model = modelEl.model;
-				const name = modelEl.sf$controlled;
-				const ref = {
-					script: modelEl.model.$el?.$scopeFunc || modelEl.sf$collection?.$scopeFunc
-				};
+			if(frameList.length !== 0){
+				component.message = "Frame Inspection Tools";
+				component.sideOpenLock = true;
+			}
 
+			for (var i = 0; i < frameList.length; i++) {
+				const frame = frameList[i];
+				const {name, nested, modelEl, model, ref} = frame;
 				console.groupCollapsed(
-				    (nested !== 0 ? `%c>> Parent frame (${nested})%c > ` : "%c>> Clicked frame%c > ") + (modelEl.sf$controlled || '{embedded template}')
+				    (nested !== 0 ? `%c>> Parent frame (${nested})%c > ` : "%c>> Clicked frame%c > ") + name
 				    , 'color:yellow', 'color:lightgreen',
-					`\n${modelEl.sf$collection ? 'Component' : 'Model'}:`, modelEl.model,
+					`\n${modelEl.sf$collection ? 'Component' : 'Model'}:`, model,
 					"\nElement:", modelEl,
 					"\nReferences:", ref
 				);
@@ -32,38 +152,12 @@ sf.dom(function(){
 					window[`PF${nested}`] = model;
 					window[`El${nested}`] = modelEl;
 				}
-
-				modelEl = modelEl.parentNode;
-				nested++;
-			};
-
-			while(nested-- !== 0)
-				console.groupEnd();
-		}
-	}, {capture:true});
-
-	const shadowEl = $('<div style="top:0;left:0;background: #47a2ff54;pointer-events:none;position:fixed"></div>');
-	$('body').append(shadowEl);
-
-	let hasShadow = false;
-	$('body').on('pointermove', function(e){
-		if(e.ctrlKey && e.altKey){
-			const el = sf(e.target, true);
-			if(!el){
-				if(hasShadow) shadowEl.css('display', 'none');
-				return;
 			}
 
-			shadowEl.css('display', '');
-			const Rect = el.getBoundingClientRect();
-			shadowEl.css('transform', `translate(${Rect.x}px, ${Rect.y}px)`);
-			shadowEl.css('width', `${Rect.width}px`);
-			shadowEl.css('height', `${Rect.height}px`);
-			hasShadow = true;
+			for (var i = 0; i < frameList.length; i++)
+				console.groupEnd();
 		}
-		else if(hasShadow){
-			shadowEl.css('display', 'none');
-			hasShadow = false;
-		}
-	});
+	}, {capture:true})
+	.on('pointerup', preventAltCtrlClick, {capture:true})
+	.on('click', preventAltCtrlClick, {capture:true});
 });
