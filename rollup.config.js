dc91791ba55c30f02fd10d6abf250ce9ef8c34ce
StefansArya <stefansarya1@gmail.com>
2021-01-18 14:54:07 +0700

    Update configurations

--- rollup.config.js
+++ rollup.config.js
@@ -10,21 +10,21 @@ if(process.env.production)
   plugins.push(terser({ output: { comments: false } }));
 
 export default [{
-  input: "src/index.no-hr.js",
+  input: "src/index.prod.js",
   output: {
     file: "dist/scarletsframe.min.js",
     sourcemap: true,
-    sourcemapFile: "dist/scarletsframe.min.js.map_",
+    sourcemapFile: "dist/scarletsframe.min.js.map",
     format: "iife",
     name: "sf"
   },
   plugins
 }, {
-  input: "src/index.js",
+  input: "src/index.dev.js",
   output: {
-    file: "dist/scarletsframe.hot.js",
+    file: "dist/scarletsframe.dev.js",
     sourcemap: true,
-    sourcemapFile: "dist/scarletsframe.hot.js.map_",
+    sourcemapFile: "dist/scarletsframe.dev.js.map",
     format: "iife",
     name: "sf"
   },
