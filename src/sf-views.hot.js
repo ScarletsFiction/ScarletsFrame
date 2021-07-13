2a1594fc4b21eb23b21bcd87ba6b8ea490f41846
StefansArya <stefansarya1@gmail.com>
2021-07-13 19:40:21 +0700

    Reuse element on development when updating views

--- src/sf-views.hot.js
+++ src/sf-views.hot.js
@@ -1,5 +1,6 @@
 // This file will be only loaded from sf-hot-reload.js
 import {Views} from "./sf-views.js";
+import {prevAll} from "./sf-dom.utils.js";
 
 Views._$edit =  function(selector, routes) {
 	let view = (Views.listSelector[selector] || new Views(selector));
@@ -35,5 +36,62 @@ Views._$edit =  function(selector, routes) {
 			relatedDOM.splice(n);
 			break;
 		}
+	}
+}
+
+Views._$edit.reuseOldElem = function(now, old){
+	let { children } = now;
+	root:for (var i = children.length - 1; i >= 0; i--) {
+		let temp = children[i];
+		let { tagName } = temp;
+
+		if(tagName === 'SF-TEMPLATE'){
+			let child = old.childNodes;
+			let path = temp.getAttribute('path');
+			if(path === null) continue;
+
+			for (var a = child.length - 1; a >= 0; a--) {
+				if(child[a].sf$templatePath === path)
+					now.insertBefore(child[a], temp);
+			}
+
+			temp.remove();
+			continue root;
+		}
+
+		let firstChild = old.firstElementChild;
+		let same = [];
+
+		if(firstChild !== null){
+			same = prevAll(firstChild, tagName, true); // true = nextAll
+
+			if(firstChild.tagName === tagName)
+				same.unshift(firstChild);
+		}
+
+		if(temp.childNodes.length === 0){
+			for (var i = same.length - 1; i >= 0; i--) {
+				let that = same[i];
+				if(that.childNodes.length === 0 || that.model !== void 0){
+					now.replaceChild(that, temp);
+					break;
+				}
+			}
+
+			continue root;
+		}
+
+		for (var i = same.length - 1; i >= 0; i--) {
+			let that = same[i];
+			if(temp.isEqualNode(that)){
+				now.replaceChild(that, temp);
+				continue root;
+			}
+		}
+
+		let last = same[same.length-1];
+		if(last.tagName === 'SF-M') continue;
+
+		Views._$edit.reuseOldElem(temp, last);
 	}
 }
